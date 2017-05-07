188add29546ef0f4e2fe1340b63f26d1af158655
Jorge Sánchez <jorge@questionity.com>
2017-05-07 23:12:11 +0200

    Upload video to youtube

--- lib/application.rb
+++ lib/application.rb
@@ -1,6 +1,16 @@
 require 'open-uri'
 require 'cgi'
 require 'net/http'
+require 'trollop'
+
+require 'rubygems'
+gem 'google-api-client', '>0.7'
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/file_storage'
+require 'google/api_client/auth/installed_app'
+
+
 require_relative 'logger'
 require_relative 'video'
 require_relative 'file_manager'
@@ -9,19 +19,21 @@ class Application
   def run
     video_id = "R2u822BzQw8"
     video = Video.new video_id
-    file_manager = FileManager.new
-    if ! file_manager.download_higher_resolution_video(video)
-      Logger::debug 'File does not exist'
-      return false
-    end
-    Logger::debug 'File exist'
-
-    if ! system(get_reverse_video_command video_id )
-      Logger::debug 'video is not reverse'
-      return false
-    end
+    # file_manager = FileManager.new
+    # if ! file_manager.download_higher_resolution_video(video)
+    #   Logger::debug 'File does not exist'
+    #   return false
+    # end
+    # Logger::debug 'File exist'
+    #
+    # if ! system(get_reverse_video_command video_id )
+    #   Logger::debug 'video is not reverse'
+    #   return false
+    # end
 
     Logger::debug 'video is reverse'
+    video.uploadVideoToYoutube
+
     return true
   end
 
--- lib/video.rb
+++ lib/video.rb
@@ -1,6 +1,9 @@
 class Video
   attr_accessor :video_id
   YOUTUBE_INFO_URI= 'http://youtube.com/get_video_info?video_id='
+  YOUTUBE_UPLOAD_SCOPE = 'https://www.googleapis.com/auth/youtube.upload'
+  YOUTUBE_API_SERVICE_NAME = 'youtube'
+  YOUTUBE_API_VERSION = 'v3'
   @video_id = nil
 
   def initialize(video_id = nil)
@@ -76,5 +79,66 @@ class Video
   def get_url_higher_resolution_video
     get_video_data_for_download['url']
   end
+
+  def uploadVideoToYoutube
+    options = getOptionsForUpload
+
+    Logger::debug options.inspect
+
+    if options[:file].nil? or not File.file?(options[:file])
+      Trollop::die :file, 'does not exist'
+    end
+
+    client, youtube = get_authenticated_service
+    Logger::debug client.inspect
+    Logger::debug youtube.inspect
+
+
+  end
+
+  def getOptionsForUpload
+    options = Trollop::options do
+      opt :file, 'Video file to upload', :type => String,
+          :default => 'downloads/R2u822BzQw8-reversed.mp4'
+      opt :title, 'Video title', :type => String,
+          :default => 'Test Title'
+      opt :description, 'Video description',
+          :default => 'Test Description', :type => String
+      opt :category_id, 'Numeric video category. See https://developers.google.com/youtube/v3/docs/videoCategories/list',
+          :default => 22, :type => :int
+      opt :keywords, 'Video keywords, comma-separated',
+          :default => '', :type => String
+      opt :privacy_status, 'Video privacy status: public, private, or unlisted',
+          :default => 'public', :type => String
+    end
+    options
+  end
+
+  def get_authenticated_service
+
+
+    client = Google::APIClient.new(
+        :application_name => $PROGRAM_NAME,
+        :application_version => '1.0.0'
+    )
+    youtube = client.discovered_api(self::YOUTUBE_API_SERVICE_NAME, self::YOUTUBE_API_VERSION)
+
+    file_storage = Google::APIClient::FileStorage.new("#{$PROGRAM_NAME}-oauth2.json")
+    if file_storage.authorization.nil?
+      client_secrets = Google::APIClient::ClientSecrets.load
+      flow = Google::APIClient::InstalledAppFlow.new(
+          :client_id => client_secrets.client_id,
+          :client_secret => client_secrets.client_secret,
+          :scope => [self::YOUTUBE_UPLOAD_SCOPE]
+      )
+      client.authorization = flow.authorize(file_storage)
+    else
+      client.authorization = file_storage.authorization
+    end
+
+    return client, youtube
+  end
+
+
 
 end
