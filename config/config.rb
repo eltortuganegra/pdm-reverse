946ee0be91c6f647ed91fdf57335eb1c726d8e8c
Jorge Sánchez <contacto@eltortuganegra.com>
2017-06-01 08:31:13 +0200

    Add runtime path

--- config/config.rb
+++ config/config.rb
@@ -1,5 +1,6 @@
 class Config
-  attr_accessor :root_path, :downloads_path, :lib_path, :clients_secrets_path, :credentials_code_path
+  attr_accessor :root_path, :downloads_path, :lib_path, :clients_secrets_path, :credentials_code_path,
+                :runtime
 
   def initialize
     Logger::debug 'Initialize config'
@@ -8,25 +9,30 @@ class Config
     setLibPath
     setClientsSecretsPath
     setCredentialsCodePath
+    setRuntimePath
+  end
+
+  def setRuntimePath
+    runtime root_path.to_s + '/runtime'
   end
 
   def setLibPath
-    @lib_path = root_path.to_s + '/lib'
+    lib_path root_path.to_s + '/lib'
   end
 
   def setDownloadsPath
-    @downloads_path = root_path.to_s + '/downloads'
+    downloads_path root_path.to_s + '/downloads'
   end
 
   def setRootPath
-    @root_path = File.absolute_path(File.dirname(__FILE__) + '/..')
+    root_path File.absolute_path(File.dirname(__FILE__) + '/..')
   end
 
   def setClientsSecretsPath
-    @clients_secrets_path = root_path.to_s + '/config/clients_secrets.json'
+    clients_secrets_path root_path.to_s + '/config/clients_secrets.json'
   end
 
   def setCredentialsCodePath
-    @credentials_code_path = root_path.to_s + '/config/credentials_code.json'
+    credentials_code_path root_path.to_s + '/config/credentials_code.json'
   end
 end
